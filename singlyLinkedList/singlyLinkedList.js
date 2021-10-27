4dcb2572dd781e9c4142a416842e479b44bace60
jeevamyil <jeevastrongest@gmail.com>
2021-10-27 23:46:42 +0530

    Add pop method

--- singlyLinkedList/singlyLinkedList.js
+++ singlyLinkedList/singlyLinkedList.js
@@ -20,5 +20,35 @@ class LinkedList {
       this.tail.next = node;
       this.tail = node;
     }
+    this.length++;
+    return this;
+  };
+  pop = () => {
+    if (!this.head) {
+      return undefined;
+    } else {
+      let currentNode = this.head;
+      let newTail = currentNode;
+      while (currentNode.next) {
+        newTail = currentNode;
+        currentNode = currentNode.next;
+      }
+      this.tail = newTail;
+      this.tail.next = null;
+      this.length--;
+      if (this.length === 0) {
+        this.head = null;
+        this.tail = null;
+      }
+      return this;
+    }
   };
 }
+
+ll = new LinkedList();
+ll.push("hi");
+ll.push("there");
+ll.push("!!!");
+console.log(ll);
+let i = ll.pop();
+console.log(ll, i);
