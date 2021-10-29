249e3b41065467dc27a921e79c105c2299a38d4f
jeevamyil <jeevastrongest@gmail.com>
2021-10-29 14:03:07 +0530

    Add reverse&print method

--- singlyLinkedList/singlyLinkedList.js
+++ singlyLinkedList/singlyLinkedList.js
@@ -125,7 +125,29 @@ class LinkedList {
       return targetNode;
     }
   };
- 
+  reverse = () => {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  };
+  print=()=>{
+    let arr = [];
+    let current = this.head
+    while(current){
+        arr.push(current.val)
+        current = current.next
+    }
+    console.log(arr);
+};
 }
 
 ll = new LinkedList();
