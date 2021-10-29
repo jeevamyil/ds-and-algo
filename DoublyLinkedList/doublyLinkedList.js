51c7878b400bde6380938ee10d443222719900c4
jeevamyil <jeevastrongest@gmail.com>
2021-10-29 14:04:46 +0530

    Add push and pop method

--- DoublyLinkedList/doublyLinkedList.js
+++ DoublyLinkedList/doublyLinkedList.js
@@ -13,7 +13,38 @@ class DLinkedList {
     this.length = 0;
   }
 
- 
+  push = (val) => {
+    let node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      let oldNode = this.tail;
+      oldNode.next = node;
+      node.prev = oldNode;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  };
+
+  pop = () => {
+    let targetNode = this.tail;
+    if (!this.head) {
+      return undefined;
+    } else if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let prevNode = targetNode.prev;
+      targetNode.prev = null;
+      prevNode.next = null;
+      this.tail = prevNode;
+    }
+    this.length--;
+    return targetNode;
+  };
+  
 }
 
 let a = new DLinkedList();
