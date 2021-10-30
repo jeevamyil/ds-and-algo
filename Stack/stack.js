a7796f07238fae4305e193dc46c0e264670a955b
jeevamyil <jeevastrongest@gmail.com>
2021-10-30 20:41:46 +0530

    Add push and pop method

--- Stack/stack.js
+++ Stack/stack.js
@@ -11,4 +11,37 @@ class Stack {
     this.last = null;
     this.size = 0;
   }
+  push = (value) => {
+    let node = new Node(value);
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      let oldHead = this.first;
+      node.next = oldHead;
+      this.first = node;
+    }
+    this.size++;
+    return this.size;
+  };
+  pop = () => {
+    if (!this.first) {
+      return null;
+    } else {
+      let targetNode = this.first;
+      this.first = targetNode.next;
+      targetNode.next = null;
+      this.size--;
+      return targetNode;
+    }
+  };
 }
+
+let a = new Stack();
+a.push(1);
+a.push(2);
+a.push(3);
+a.push(4);
+
+let b = a.pop();
+console.log(a, b);
