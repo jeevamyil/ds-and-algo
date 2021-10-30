c583c96b6aef06cd7e334dd9aaed201a651f2014
jeevamyil <jeevastrongest@gmail.com>
2021-10-30 20:44:15 +0530

    Add insert method

--- BST/BinarySearchTree.js
+++ BST/BinarySearchTree.js
@@ -10,4 +10,40 @@ class BST {
   constructor() {
     this.root = null;
   }
+  insert = (value) => {
+    let node = new Node(value);
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) {
+        return undefined;
+      } else if (value < current.value) {
+        if (!current.left) {
+          current.left = node;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (value > current.value) {
+          if (!current.right) {
+            current.right = node;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }
+  };
+ 
 }
+
+let a = new BST();
+
+a.insert(10);
+a.insert(5);
+a.insert(33);
+a.insert(7);
+console.log(JSON.stringify(a), a.find(33), a.contains(33), a.contains(234));
