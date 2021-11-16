41658b7eec26f19fba4b46cf61686ceb6419338b
jeevamyil <jeevastrongest@gmail.com>
2021-11-17 00:00:09 +0530

    Add removeEdge and removeVertex method

--- Graph/graph.js
+++ Graph/graph.js
@@ -12,4 +12,17 @@ class Graph {
     this.adjacenyList[v1].push(v2);
     this.adjacenyList[v2].push(v1);
   };
+
+  removeEdge = (v1, v2) => {
+    this.adjacenyList[v1] = this.adjacenyList[v1].filter((v) => v !== v2);
+    this.adjacenyList[v2] = this.adjacenyList[v2].filter((v) => v !== v1);
+  };
+
+  removeVertex =(vertex)=>{
+    while(this.adjacencyList[vertex].length){
+        const adjacentVertex = this.adjacencyList[vertex].pop();
+        this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex]
+  }
 }
