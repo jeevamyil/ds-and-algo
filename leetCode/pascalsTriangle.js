11d428594928188d006f70a184ee3f5bf0146898
jeevamyil <jeevastrongest@gmail.com>
2021-11-30 23:39:58 +0530

    Add pascalTraingle

---
+++ leetCode/pascalsTriangle.js
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/reshape-the-matrix/submissions/
+
+var generate = function (numRows) {
+  let result = [];
+  for (let i = 0; i < numRows; i++) {
+    result[i] = [];
+    result[i][0] = 1;
+    result[i][i] = 1;
+    for (let j = 1; j < i; j++) {
+      result[i][j] = result[i - 1][j] + result[i - 1][j - 1];
+    }
+  }
+  return result;
+};
+
+console.log(generate(5));
