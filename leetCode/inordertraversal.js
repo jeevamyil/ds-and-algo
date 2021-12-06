b8909d5691d1eea022295182c632b70d8fc0687e
jeevamyil <jeevastrongest@gmail.com>
2021-12-06 23:56:20 +0530

    Add inorder traversal

---
+++ leetCode/inordertraversal.js
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+var inorderTraversal = function(root) {
+    let arr = visitNode(root, []);
+  return arr;
+};
+
+var visitNode = function(root, arr){
+   if (!root) {
+       return [];
+   }
+   
+   
+   
+   if (root.left){
+       visitNode(root.left, arr);
+   }
+   
+   if (root.val){
+       arr.push(root.val);
+   }
+   
+   if (root.right){
+       visitNode(root.right, arr);
+   }
+   
+   return arr;
+}
