8f392a227746bd89551f753d7186209bcf9be5e4
jeevamyil <jeevastrongest@gmail.com>
2021-12-07 23:57:32 +0530

    Add level order

---
+++ leetCode/levelOrder.js
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+var levelOrder = function(root) {
+    if (root == null) return [];
+    let queue = [], values = [];
+    queue.push(root);
+    while (queue.length > 0) {
+        let len = queue.length, level = [];
+        for (let i = 0; i < len; i++) {
+            let node = queue.shift();
+            level.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        values.push(level);
+    }
+    return values;
+};
