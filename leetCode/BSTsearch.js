b98b9323aa78b07f8c20f2f4f1e569c88404c720
jeevamyil <jeevastrongest@gmail.com>
2021-12-09 01:22:25 +0530

    Add BSTsearch

---
+++ leetCode/BSTsearch.js
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/search-in-a-binary-search-tree/submissions/
+
+var searchBST = function (root, val) {
+  if (!root) return null;
+  if (root.val === val) return root;
+  if (root.val > val) {
+    return searchBST(root.left, val);
+  } else {
+    return searchBST(root.right, val);
+  }
+};
