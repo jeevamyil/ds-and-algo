efb02f54ecb11211a481d0bd0d2b403ace211e0f
jeevamyil <jeevastrongest@gmail.com>
2021-12-18 23:59:17 +0530

    Add repeatedDNA sequence

--- leetCode/repeatedDNAsequence.js
+++ leetCode/repeatedDNAsequence.js
@@ -4,8 +4,11 @@ var findRepeatedDnaSequences = function (s) {
   const res = new Set();
   for (let i = 10; i < s.length; i++) {
     curr = curr.slice(1) + s[i];
+    console.log(curr)
     if (seen.has(curr)) res.add(curr);
     seen.add(curr);
   }
   return [...res];
 };
+
+console.log(findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
